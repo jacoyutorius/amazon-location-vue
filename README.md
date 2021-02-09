fe3140705fa9a3664016f565a49aa1489046b293
jacoyutorius <jacoyutorius@gmail.com>
2021-02-10 01:18:33 +0900

    update readme, get default location from navigator.geolocation

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # amplify-location-services-maps
 
+here is a sample of Amazon Location Service with Vue.
+thanks for https://www.youtube.com/watch?v=-QcEMHqndzw.
+
 ![](./amplify-location-services-maps.png)
+
+```
+$ vue create amplify-location-services-maps
+$ cd amplify-location-services-maps
+$ npm install aws-sdk aws-amplify
+$ amplify init
+$ amplify add auth
+$ amplify push 
+
+setup following on Management Console
+- Enable to access to unauthorized identities
+- apply inline policy to Amazon Location Service
+```
+
+---
 
 ## Project setup
 ```
--- src/components/MapPane.vue
+++ src/components/MapPane.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="mappane">
     <h2>Amazon location service with Vue</h2>
+
+    <div class="search">
+      <form>
+        <input type="text" v-model="search.place" placeholder="Search place here">
+        <button type="button" @click="getCoding">Search</button>
+      </form>
+    </div>
     <div id="map"></div>
   </div>
 </template>
 
 <script>
-/* eslint no-unused-vars: 0, no-undef: 0 */
 import mapboxgl from "mapbox-gl";
 
-import { AWS } from "aws-sdk";
 import { Auth } from 'aws-amplify';
 import { Signer } from "@aws-amplify/core";
 import Location from "aws-sdk/clients/location";
@@ -19,30 +24,65 @@ export default {
   name: "MapPane",
   data() {
     return {
-      credentials: null
+      credentials: null,
+      search: {
+        place: ""
+      },
+      map: null,
+      currentPosition: {
+        longitude: 143.767125, // default
+        latitude: 38.681236
+      },
+      location: []
     };
   },
-  mounted: function() {
+  mounted: async function() {
+    const currentPosition = await this.getCurrentPosition();
+    this.currentPosition = {
+      longitude: currentPosition.coords.longitude,
+      latitude: currentPosition.coords.latitude,
+    };
+
     this.mapCreate();
   },
+  computed: {
+    viewPort: function() {
+      return [this.currentPosition.longitude, this.currentPosition.latitude];
+    }
+  },
   methods: {
+    getCurrentPosition(options) {
+      if (!navigator.geolocation) {
+        alert('このブラウザ/OSではジオロケーションに対応していません');
+        return null;
+      }
+
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+      })
+    },
     async mapCreate() {
       const credentials = await Auth.currentCredentials();
       this.credentials = credentials;
 
-      const map = new mapboxgl.Map({
+      this.map = new mapboxgl.Map({
         container: "map",
-        center: [-123.1187, 49.2819], // initial map centerpoint
-        // center: [34.6989, 137.7000],
-        zoom: 10, // initial map zoom
+        center: this.viewPort,
+        zoom: 10.5,
         style: "MyMap",
         transformRequest: this.transformRequest,
       });
 
-      console.log({map});
-
       // コントロール関係表示
-      map.addControl(new mapboxgl.NavigationControl(), "top-left");
+      this.map.addControl(
+        new mapboxgl.NavigationControl(),
+        "top-left");
+
+      // マーカー表示
+      new mapboxgl.Marker({
+          color: "red"
+        }).setLngLat(this.viewPort)
+          .addTo(this.map);
     },
     transformRequest(url, resourceType) {
       if (resourceType === "Style" && !url.includes("://")) {
@@ -62,6 +102,38 @@ export default {
       }
 
       return { url };
+    },
+    async getCoding() {
+      const client = new Location({
+        credentials: this.credentials,
+        region: awsconfig.aws_project_region
+      });
+      const params = {
+        IndexName: "explore.place",
+        Text: this.search.place
+      };
+
+      const data = await client.searchPlaceIndexForText(params)
+                               .promise()
+                               .catch(e => console.log(e));
+
+      // TODO: geolocationにて取得した位置情報をcenterにして地図を再描画する方法がわかんないー
+      if (data) {
+        const coordinates = data.Results[0].Place.Geometry.Point;
+
+        this.currentCredentials = {
+          longitude: coordinates[0],
+          latitude: coordinates[1]
+        }
+
+        new mapboxgl.Marker({
+          color: "blue",
+          draggable: true
+        }).setLngLat([coordinates[0], coordinates[1]])
+          .addTo(this.map);
+
+        // console.log({marker});
+      }
     }
   }
 }
